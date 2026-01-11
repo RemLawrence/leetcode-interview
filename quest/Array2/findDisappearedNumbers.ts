677c002347091da118958e438e7d5b68e3f183a6
Micah.W <46633445+RemLawrence@users.noreply.github.com>
2026-01-11 17:31:25 -0400

    Create findDisappearedNumbers.ts

---
+++ quest/Array2/findDisappearedNumbers.ts
@@ -0,0 +1,14 @@
+function findDisappearedNumbers(nums: number[]): number[] {
+    const result = Array(nums.length + 1).fill(0);
+    for (const num of nums) {
+        result[num]++;
+    }
+
+    const resultingArray = [];
+    for (let i = 1; i < nums.length + 1; i++) {
+        if (result[i] === 0) {
+            resultingArray.push(i);
+        }
+    }
+    return resultingArray;
+};
