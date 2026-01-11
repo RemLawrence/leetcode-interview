26727ee149249f60707bb731766c4e1a1b7fc1e5
Micah.W <46633445+RemLawrence@users.noreply.github.com>
2026-01-11 16:43:36 -0400

    Add smallerNumbersThanCurrent function
    Implement function to count smaller numbers for each element.

---
+++ quest/Array2/smallerNumbersThanCurrent.ts
@@ -0,0 +1,21 @@
+function smallerNumbersThanCurrent(nums: number[]): number[] {
+    // Transform
+    const numsMap = nums.map((value, index) => ({value, index}));
+
+    // Sort
+    numsMap.sort((a, b) => a.value - b.value);
+
+    // Prepare the result array
+    const result = Array(nums.length);
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        // numsMap[i].index // original index
+        // i // current index
+        // result[i] // result to fill in
+        if (i > 0 && numsMap[i].value !== numsMap[i-1].value) {
+            count = i;
+        }
+        result[numsMap[i].index] = count;
+    }
+    return result;
+};
